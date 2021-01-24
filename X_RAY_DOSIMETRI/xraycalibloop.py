c8eb6d066e3201dd621e90bb2fa0e88a747cad36
Henrik Mortensen <vorup84@gmail.com>
2021-01-25 00:00:56 +0100

    Kan nu udregne norm uden at tage egen værdi med i gennemsnittet

--- X_RAY_DOSIMETRI/xraycalibloop.py
+++ X_RAY_DOSIMETRI/xraycalibloop.py
@@ -35,43 +35,11 @@ Det betyder, at forsøget altid skal laves med samme antal målinger i hvert
 punkt,hvis numpy.loadtxt() skal kunne klare at læse filen.
 
 
-Lidt teori:
-Hvornår skal jeg bruge standardafvig (SD = std()) og hvornår (HVORDAN) skal jeg bruge
-standard error of the mean (SEM = SD/√n)?
+Spørgsmål: Hvordan inplementerer jeg fejlen i estimatet for
+kalibreringsfaktoren: (∆D/D)2 = (∆C/C)2 + (∆K/K)2
 
-Investopedia.com skriver:
+Alle målinger af CHRG er estimeret til ±0.02
 
-"The standard deviation (SD) measures the amount of variability, or dispersion,
-from the individual data values to the mean, while the standard error of the
-mean (SEM) measures how far the sample mean (average) of the data is likely to
-be from the true population mean. The SEM is always smaller than the SD.
-
-KEY TAKEAWAYS
-Standard deviation (SD) measures the dispersion of a dataset relative to its
-mean.
-Standard error of the mean (SEM) measured how much discrepancy there is likely
-to be in a sample's mean compared to the population mean.
-The SEM takes the SD and divides it by the square root of the sample size.
-
-SEM vs. SD
-Standard deviation and standard error are both used in all types of statistical
-studies, including those in finance, medicine, biology, engineering, psychology,
-etc. In these studies, the standard deviation (SD) and the estimated standard
-error of the mean (SEM) are used to present the characteristics of sample data
-and to explain statistical analysis results. However, some researchers
-occasionally confuse the SD and SEM. Such researchers should remember that the
-calculations for SD and SEM include different statistical inferences, each of
-them with its own meaning. SD is the dispersion of individual data values.
-
-In other words, SD indicates how accurately the mean represents sample data.
-However, the meaning of SEM includes statistical inference based on the
-sampling distribution. SEM is the SD of the theoretical distribution of the
-sample means (the sampling distribution)."
-
-
-Bemærkninger:
-Jeg bør nok køre mindst ti målinger i hvert punkt siden nogle af målingerne ikke
-holder sig indenfor standardafviget.
 """
 
 import numpy as np
@@ -89,6 +57,10 @@ pu = 1.02               #perturbation factor
 kTP = 1.018             #corrects for the change in temperature and pressure from the calibration to the measurement
 NK = 43.77              #air kerma calibration factor for the given beam quality
 K = NK*ku*muen_over_rho*pu*kTP
+
+DeltaNK = 0.39
+DeltaC = 0.02
+
 
 print(100/K)
 
@@ -111,6 +83,8 @@ y_lines = int(y.max() + 1)  #antal linjer med målepunkter = 6
 N = y_lines*x_points        #antal målingspunkter på perspex-pladen = 30 (kan også være flere)
 n = int(f.max() + 1)        #antal forsøg gjort ( = 3 hvis færdig)
 
+
+
 print("m[0] =", m[0])
 print("m.shape[0] =", m.shape[0])
 print("m.shape[1] =", m.shape[1])
@@ -123,10 +97,10 @@ print ("n =", n)
 #### REGN GENNEMSNITTET AF MÅLINGERNE I HVERT PUNKT OG OMREGN TIL DOSIS ####
 ya = np.zeros(x_points)                     #dosisgennemsnit for hver y-linje
 yo = np.zeros(x_points)                     #standardafvig for hver y-linje
-a_matrix = np.zeros((y_lines, x_points))    #5x6-matrice med dosisgennemsnittene som rækker
-o_matrix = np.zeros((y_lines, x_points))    #5x6-matrice med standardafvigene som rækker
-A_matrix = np.zeros((n, y_lines, x_points)) #3x6x5-matrice med alle forsøgs dosisgennemsnit
-O_matrix = np.zeros((n, y_lines, x_points)) #3x6x5-matrice med alle forsøgs standardafvig
+a_matrix = np.zeros( (y_lines, x_points) )    #5x6-matrice med dosisgennemsnittene som rækker
+o_matrix = np.zeros( (y_lines, x_points) )    #5x6-matrice med standardafvigene som rækker
+A_matrix = np.zeros( (n, y_lines, x_points) ) #3x6x5-matrice med alle forsøgs dosisgennemsnit
+O_matrix = np.zeros( (n, y_lines, x_points) ) #3x6x5-matrice med alle forsøgs standardafvig
 for i in range(n): #3                               #gå igennem alle forsøg
     for j in range(y_lines): #6                     #gå igennem alle rækker
         for k in range(x_points): #5                #gå igennem alle målepunkter
@@ -139,29 +113,29 @@ for i in range(n): #3                               #gå igennem alle forsøg
 
 #### MATRIX MED GENNEMSNITSDOSIS OG STANDARD ERROR
 Mean_Of_Total = np.zeros(N)
-SE = np.zeros(N)
 Mean_Of_Means = np.zeros(N)
+SE = np.zeros(N)
 SEM = np.zeros(N)
 
-AllPoints = np.zeros((n,m.shape[1])) #samtlige enkeltmålinger i hvert punkt
+AllPoints = np.zeros( (n, m.shape[1]) ) #samtlige enkeltmålinger i hvert punkt
 
-for i in range(N):                                              #gå igennem alle målepunkter: range(N) = 0,1,2,...29
-    PointMeans = np.zeros(n)                                #for hvert målepunkt; lav en 3-array med hver alle målinger i samme punkt
-    for j in range(n):                                          #gå igennem alle målinger i hvert punkt: range(n) = 0,1,2
+for i in range(N):                                  # gå igennem alle målepunkter: range(N) = 0,1,2,...29
+    PointMeans = np.zeros(n)                        # for hvert målepunkt; lav en 3-array med hver alle målinger i samme punkt
+    for j in range(n):                              # gå igennem alle målinger i hvert punkt: range(n) = 0,1,2
         AllPoints[j] = m[i + N*j]
-        PointMeans[j] = np.mean(m[i + N*j])                 #fyld 3-array med alle gennemsnit i samme punkt
-    Mean_Of_Total[i] = np.mean(AllPoints)                      #lav 30-array med gennemsnit fra samtlige målinger
-    SE[i] = np.std(AllPoints)/np.sqrt(n*m.shape[1])  #lav 30-array med SEM i hvert målepunkt SEM=std(all_meas)/sqrt(30)
-    Mean_Of_Means[i] = np.mean(PointMeans)                       #lav 30-array med gennemsnit af gennemsnitsdoser i hvert målepunkt
-    SEM[i] = np.std(PointMeans)/np.sqrt(n)             #lav 30-array med SEM i hvert målepunkt SEM=std(all_avgs)/sqrt(3)
+        PointMeans[j] = np.mean(m[i + N*j])         # fyld n-array med alle gennemsnit i samme punkt
+    Mean_Of_Total[i] = np.mean(AllPoints)           # lav N-array med gennemsnit fra samtlige målinger
+    Mean_Of_Means[i] = np.mean(PointMeans)          # lav 30-array med gennemsnit af gennemsnitsdoser i hvert målepunkt
+    SE[i] = np.std(AllPoints)/np.sqrt(n*m.shape[1]) # lav 30-array med SEM i hvert målepunkt SEM=std(all_meas)/sqrt(30)
+    SEM[i] = np.std(PointMeans)/np.sqrt(n)          # lav 30-array med SEM i hvert målepunkt SEM=std(all_avgs)/sqrt(3)
 
 y_all = np.zeros(x_points)
-y_se = np.zeros(x_points)
-DoseMatrix_a = np.zeros((y_lines, x_points))
-SE_Matrix = np.zeros((y_lines, x_points))
 y_means = np.zeros(x_points)
+y_se = np.zeros(x_points)
 y_sem = np.zeros(x_points)
-DoseMatrix_m = np.zeros((y_lines, x_points))
+DoseMatrix_all = np.zeros((y_lines, x_points))
+DoseMatrix_mean = np.zeros((y_lines, x_points))
+SE_Matrix = np.zeros((y_lines, x_points))
 SEM_Matrix = np.zeros((y_lines, x_points))
 
 for i in range(y_lines):
@@ -170,25 +144,31 @@ for i in range(y_lines):
         y_se[j]  = SE[j + i*(x_points)]
         y_means[j] = Mean_Of_Means[j + i*(x_points)]
         y_sem[j] = SEM[j + i*(x_points)]
-    DoseMatrix_a[i] = y_all
+    DoseMatrix_all[i] = y_all
+    DoseMatrix_mean[i] = y_means
     SE_Matrix[i] = y_se
-    DoseMatrix_m[i] = y_means
     SEM_Matrix[i] = y_sem
 
-if DoseMatrix_a.any() != DoseMatrix_m.any():
-    print('Error in calculation of SE and/or SEM')
+#### Sikkerhed for at SE og SEM er rigtig udregnet ####
+if DoseMatrix_all.any() != DoseMatrix_mean.any():
+    print('ADVARSEL!! Gennemssnitsdose udregent fra alle målinger ≠ gennemsnitsdose udregnet fra gennemsnittene')
+
+
 
 #### NORMALISERING AF ALLE MÅLINGSINDEKS
 #### FOR AT SE HVOR MEGET MERE RØRET GIVER
 #### I DEN FØRSTE MÅLING
-nrm = np.zeros(m.shape)
-nd = np.zeros((m.shape[1],m.shape[0]))
-for i in range(m.shape[0]):     # hele datasættet igennem (90 punkter)
-    nrm[i] = m[i]/np.mean(m[i])
-    for j in range(m.shape[1]): # 5 gange, dvs en gang pr målingsindeks
-        nd[j,i] = nrm[i,j]
+norm = np.zeros(m.shape)
+norm_wi = np.zeros(m.shape)
+NormArray = np.zeros( (m.shape[1], m.shape[0]) )
+NormArray_wi = np.zeros( (m.shape[1], m.shape[0]) )
 
-
+for i in range(m.shape[0]):                                                 # hele datasættet igennem (90 punkter)
+    norm[i] = m[i]/(np.mean(m[i]))                                          # normaliser alle målinger ift eget punktsæt
+    for j in range(m.shape[1]):                                             # 5 eller 10 gange, dvs en gang pr målingsindeks
+        norm_wi[i] = m[i]/np.mean(np.concatenate((m[i][:j], m[i][j+1:])))   # normaliser måling ift resterende målinger (ikke egen måling)
+        NormArray[j,i] = norm[i,j]                                          # array til plotting
+        NormArray_wi[j,i] = norm_wi[i,j]                                    # array til plotting
 
 #### PLOTTING ####
 # plt.rcParams.update({'font.size': 9})
@@ -238,16 +218,14 @@ else:
                 axs[0,i].text(j, k, '%.2f' %c, va='bottom', ha='center', fontsize=f)
                 axs[0,i].text(j, k, "±"'%.2f' %d, va='top', ha='center', fontsize=f)
                 axs[1,i].text(j, k, '%.2f' %d, va='center', ha='center', fontsize=f)
-
-
-
+plt.tight_layout(rect=[0, 0.03, 1, 0.95])
 
 # plt.show()
 
 #plot gennemsnitsdose og SEM fra alle forsøg
-fig, ax = plt.subplots(nrows=1,ncols=3,figsize=(10.2, 5))
+fig, ax = plt.subplots(nrows=1,ncols=3,figsize=(9.55, 5))
 fig.suptitle("Gennemsnitsdosis og standard error, %s $\\times$ %s målinger pr punkt, %s, %ss"%(n, nx1, SSD, t), fontsize=14)
-ax[0].imshow(DoseMatrix_m, vmin=A_min, vmax=A_max, cmap='inferno',interpolation='lanczos')
+ax[0].imshow(DoseMatrix_mean, vmin=A_min, vmax=A_max, cmap='inferno',interpolation='lanczos')
 ax[0].set_title("Gennemsnitsdosis (mGy)")
 ax[0].invert_yaxis()
 ax[1].imshow(SE_Matrix, vmin=O_min, vmax=O_max, cmap=plt.cm.Blues,interpolation='lanczos')
@@ -258,53 +236,90 @@ ax[2].set_title("Standard error of the mean (mGy),\n $SEM=\\frac{np.std([\\overl
 ax[2].invert_yaxis()
 for i in range(x_points):
     for j in range(y_lines):
-        c = DoseMatrix_m[j,i]
+        c = DoseMatrix_mean[j,i]
         d = SE_Matrix[j,i]
         e = SEM_Matrix[j,i]
         g = np.array([c, d, e])
         for k in range(3):
             ax[k].text(i, j, '%.2f' %g[k], va='center', ha='center', fontsize=f)
+plt.tight_layout(rect=[0, 0.03, 1, 0.93])
+
+
+
+
+
+
 
 #### PLOT FOR AT SE HVOR MEGET ENKELTE MÅLINGER AFVIGER FRA GENNEMSNITTET
 
+tito = "Målingsindeks normaliseret i forhold til gennemsnittet i hvert punktmålingssæt, %s, %ss"
 ylimits = (0.91, 1.09)
+size = n*N
 if m.shape[1] <= 5:
     figo, axo = plt.subplots(nrows=1,ncols=5,figsize=(14, 6), sharey="all")
-
-    # figo.suptitle("Målingsindeks i forhold til kohortgennemsnittet, %s, %ss"%(SSD, t), fontsize=14)
+    figo.suptitle(tito%(SSD, t), fontsize=14)
     for i in range(m.shape[1]):
-        k = np.mean(nd[i])
+        k = np.mean(NormArray[i])
         axo[i].axhline(y=k,label="gennemsnit: %.4f"%k, color="red")
-        axo[i].plot(nd[i], ".", label="målinger")
+        axo[i].plot(NormArray[i], ".", label="målinger")
         axo[i].set_title("alle %s.-målinger"%(i+1))
         axo[i].legend(loc=9, fontsize=f)
         axo[i].set_ylim((ylimits))
+        axo[i].set_xticks(np.linspace(0,int(n*N),10))
 else:
     figo, axo = plt.subplots(nrows=2,ncols=5,figsize=(14, 8), sharey="all")
-    # figo.suptitle("Målingsindeks i forhold til kohortgennemsnittet, %s, %ss"%(SSD, t), fontsize=14)
+    figo.suptitle(tito%(SSD, t), fontsize=14)
     for i in range(2):
         for j in range(5):
-            k = np.mean(nd[j + i*5])
+            k = np.mean(NormArray[j + i*5])
             axo[i,j].axhline(y=k,label="gennemsnit: %.4f"%k, color="red")
-            axo[i,j].plot(nd[j + i*5], ".", label="målinger")
+            axo[i,j].plot(NormArray[j + i*5], ".", label="målinger")
             axo[i,j].set_title("alle %s.-målinger"%(j + 1 + i*5))
             axo[i,j].legend(loc=9, fontsize=f)
             axo[i,j].set_ylim(ylimits)
             axo[i,j].set_xlabel("Målingsrække")
             axo[i,j].set_ylabel("Normaliseret måling")
-plt.tight_layout()
+            axo[i,j].set_xticks(np.linspace(0,int(n*N),7))
+plt.tight_layout(rect=[0, 0.03, 1, 0.95])
 
+
+
+
+titi = "Første måling normailseret i forhold til\ngennemsnittet i hvert punktmålingssæt, %s, %ss"
 figi, axi = plt.subplots()
-axi.plot(nd[0], ".", label="normaliseret måling")
+axi.plot(NormArray[0], ".", label="normaliseret måling")
 axi.set_ylim(ylimits)
 axi.set_xlabel("Målingsindeks")
 axi.set_ylabel("Normaliseret måling")
-k = np.mean(nd[0])
+k = np.mean(NormArray[0])
 axi.axhline(y=k,label="gennemsnitlig førstemåling ift. gennemsnittet: %.4f"%k, color="red")
-axi.set_title("Første måling i hver kohort delt på kohortgennemsnittet")
+axi.set_title(titi%(SSD, t))
 axi.legend()
-plt.tight_layout()
-plt.xticks(np.linspace(0,90,19))
+plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+if n == 1:
+    plt.xticks(np.linspace(0,int(n*N),7))
+else:
+    plt.xticks(np.linspace(0,int(n*N),19))
+
+
+
+titw = "Første måling normailseret i forhold til\ngennemsnittet af de resterende målinger\ni hvert punktmålingssæt, %s, %ss"
+figw, axw = plt.subplots()
+axw.plot(NormArray_wi[0], ".", label="normaliseret måling")
+axw.set_ylim(ylimits)
+axw.set_xlabel("Målingsindeks")
+axw.set_ylabel("Normaliseret måling")
+k_wi = np.mean(NormArray_wi[0])
+axw.axhline(y=k_wi,label="gennemsnitlig førstemåling ift. gennemsnittet: %.4f"%k_wi, color="red")
+axw.set_title(titw%(SSD, t))
+axw.legend()
+plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+if n == 1:
+    plt.xticks(np.linspace(0,int(n*N),7))
+else:
+    plt.xticks(np.linspace(0,int(n*N),19))
+
+
 plt.show()
 
 
