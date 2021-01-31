29977a8cf8df84025bbac87117ea9dc772d6df09
hvmortensen <vorup84@gmail.com>
2021-01-31 14:16:40 +0100

    SE og SEM modificeret til at medregne fejlen ∆D

--- X_RAY_DOSIMETRI/xraycalibloop.py
+++ X_RAY_DOSIMETRI/xraycalibloop.py
@@ -66,24 +66,24 @@ kTP = 1.018                         #corrects for the change in temperature and 
 NK = 43.77                          #air kerma calibration factor for the given beam quality
 K = NK*ku*muen_over_rho*pu*kTP
 
-
+DNK = 0.39
+DC = 0.02
 
 #### VÆLG TXT.-FIL SOM SKAL KØRES ####
 # filename = 'xray_data_SSD50_20s.txt'; t = 20; SSD = "SSD50"
 # filename = 'xray_data_SSD40_14s.txt'; t = 14; SSD = "SSD40"
-# filename = 'xray_data_SSD40_13s.txt'; t = 13; SSD = "SSD40"
-filename = 'xray_data_SSD37_5_12s.txt'; t = 12; SSD = "SSD37_5"
+filename = 'xray_data_SSD40_13s.txt'; t = 13; SSD = "SSD40"
+# filename = 'xray_data_SSD37_5_12s.txt'; t = 12; SSD = "SSD37_5"
 
+# sortér data i kolonner
 data = np.loadtxt(filename)
-
-#sortér data i kolonner
 f = data[0:,0]      # første kolonne = eksperiment nummer
 y_ = data[0:,1]     # anden kolonne = y-kordinaten
 x_ = data[0:,2]     # tredje kolonne = x-koordinaten
 C = data[:,3:]      # fjerde til sidste kolonne = aflæst CHRG (nC)
 D = data[:,3:]*K    # CHRG --> Dose ... nC --> Gy
 
-#nogle praktiske størrelser
+# nogle praktiske størrelser
 x = int(x_.max() + 1)   # antal målepunkter pr linje på perspexpladen = 5
 y = int(y_.max() + 1)   # antal linjer med 5 målepunkter på perspexpladen = 6
 N = y*x                 # antal målingspunkter på perspex-pladen = 30 (kan også være flere)
@@ -92,48 +92,56 @@ m = C.shape[1]          # antal målinger pr punkt
 
 
 #### UDREGN FEJLEN I D (∆D/D)^2 = (∆C/C)**2 + (∆K/K)**2 I HVERT MÅLEPUNKT ####
-DNK = 0.39
-DC = 0.02
+# def DD(c_,d_):
+#     return np.sqrt( (DC/c_)**2 + (DNK/NK)**2 )*d_
 DD = np.zeros_like(C)
 for i in range(N*n):
     for j in range(m):
         DD[i,j] = np.sqrt( (DC/C[i,j])**2 + (DNK/NK)**2 )*D[i,j]
 
-print (D)
 
 #### UDREGN GENNEMSNITTET AF MÅLINGERNE I HVERT PUNKT OG OMREGN TIL DOSIS ####
-yd = np.zeros(x)                    # dosisgennemsnit for hver y-linje
-ys = np.zeros(x)                    # standardafvig for hver y-linje
-d_matrix = np.zeros( (y, x) )       # 6x5-matrice med dosisgennemsnittene som rækker
-s_matrix = np.zeros( (y, x) )       # 6x5-matrice med standardafvigene som rækker
-D_Matrix = np.zeros( (n, y, x) )    # 3 x 6x5-matrice med hvert enkelt forsøgs dosisgennemsnit i hver sin matrice
-S_Matrix = np.zeros( (n, y, x) )    # 3 x 6x5-matrice med hvert enkelt forsøgs standardafvig i hver sin matrice
+yd = np.zeros(x)                                # 5-array: dosisgennemsnit for hver y-linje
+ys = np.zeros(x)                                # 5-array: standardafvig for hver y-linje
+d_matrix = np.zeros( (y, x) )                   # 6x5-matrice: dosisgennemsnittene som rækker
+s_matrix = np.zeros( (y, x) )                   # 6x5-matrice: standardafvigene som rækker
+D_Matrix = np.zeros( (n, y, x) )                # 3 6x5-matricer med hvert enkelt forsøgs dosisgennemsnit i hver sin matrice
+S_Matrix = np.zeros( (n, y, x) )                # 3 6x5-matricer med hvert enkelt forsøgs standardafvig i hver sin matrice
+
+# udregn gnsn og SD og fyld matricer til PLOTTING
 for i in range(n):                              # gå igennem alle forsøg
     for j in range(y):                          # gå igennem alle rækker
         for k in range(x):                      # gå igennem alle målepunkter
-            yd[k] = np.mean(D[k + j*x + i*N])
-            ys[k] = np.std(D[k + j*x + i*N])
-        d_matrix[j] = yd
-        s_matrix[j] = ys
-    D_Matrix[i] = d_matrix
-    S_Matrix[i] = s_matrix
+            yd[k] = np.mean(D[k + j*x + i*N])   # gennemsnit af hvert punktsæt
+            ys[k] = np.std(D[k + j*x + i*N])    # standardafvig af hvert punktsæt
+        d_matrix[j] = yd                        # 6x5-matrice med gennemsnit
+        s_matrix[j] = ys                        # 6x5-matrice med standardafvig
+    D_Matrix[i] = d_matrix                      # 3 6x5 matricer med gennemsnit til PLOTTING
+    S_Matrix[i] = s_matrix                      # 3 6x5 matricer med standardafvig til PLOTTING
 
-#### MATRIX MED GENNEMSNITSDOSIS OG STANDARD ERROR
+#### MATRICER MED GENNEMSNITSDOSIS, SE, OG SEM
 Mean_Of_Total = np.zeros(N)
 Mean_Of_Means = np.zeros(N)
 SE = np.zeros(N)
 SEM = np.zeros(N)
 AllPoints = np.zeros( (n, m) )              #samtlige enkeltmålinger i hvert punkt
+AllErrors = np.zeros( (n, m) )              #samtlige enkeltmålingers standardfejl i hvert punkt
 
-for i in range(N):                          # gå igennem alle målepunkter på perspexpladen: range(N) = 0,1,2,...29
-    PointMeans = np.zeros(n)                # for hvert målepunkt; lav en 3-array med hver alle målinger i samme punkt
-    for j in range(n):                      # gå igennem alle målinger i hvert punkt: range(n) = 0,1,2
-        AllPoints[j] = D[i + N*j]           # alle målinger fra alle forsøg
-        PointMeans[j] = np.mean(D[i + N*j]) # fyld n-array med alle gennemsnit i samme punkt
-    Mean_Of_Total[i] = np.mean(AllPoints)   # udregn gennemsnit lav N-array med gennemsnit fra samtlige målinger
-    Mean_Of_Means[i] = np.mean(PointMeans)  # udregn gennemsnit lav N-array med gennemsnit af gennemsnitsdoser i hvert målepunkt
-    SE[i] = np.std(AllPoints)/np.sqrt(n*m)  # udregn SE og lav N-array med SEM i hvert målepunkt
-    SEM[i] = np.std(PointMeans)/np.sqrt(n)  # udregn SEM og lav N-array med SEM i hvert målepunkt
+# udregn gnsn, SE, og SEM
+for i in range(N):                                          # gå igennem alle målepunkter på perspexpladen: range(N) = 0,1,2,...29
+    PointMeans = np.zeros(n)                                # for hvert målepunkt; lav en 3-array med hver alle målinger D i samme punkt
+    PointErrors = np.zeros(n)                               # for hvert målepunkt; lav en 3-array med hver alle standardfejl ∆D i samme punkt
+    for j in range(n):                                      # gå igennem alle målinger i hvert punkt: range(n) = 0,1,2
+        AllPoints[j] = D[i + N*j]                           # alle enkeltmålinger fra alle forsøg
+        AllErrors[j] = DD[i + N*j]                          # alle enkeltmålingers standardfejl fra alle forsøg
+        PointMeans[j] = np.mean(D[i + N*j])                 # fyld n-array med alle gennemsnitsmålinger i samme punkt for hver n
+        PointErrors[j] = np.mean(DD[i + N*j])               # fyld n-array med alle gennemsnits-standardfejl i samme punkt for hver n
+    Mean_Of_Total[i] = np.mean(AllPoints)                   # udregn gennemsnit lav N-array med gennemsnit fra samtlige målinger
+    Mean_Of_Means[i] = np.mean(PointMeans)                  # udregn gennemsnit lav N-array med gennemsnit af gennemsnitsdoser i hvert målepunkt
+    SE[i] = np.sqrt( (np.std(AllPoints)/np.sqrt(n*m))**2\
+                    + np.mean(AllErrors)**2 )               # udregn SE og lav N-array med SEM i hvert målepunkt
+    SEM[i] = np.sqrt( (np.std(PointMeans)/np.sqrt(n))**2\
+                    + np.mean(PointErrors)**2 )             # udregn SEM og lav N-array med SEM i hvert målepunkt
 
 ya = np.zeros(x)
 ym = np.zeros(x)
@@ -144,6 +152,7 @@ DM_Matrix = np.zeros( (y, x) )
 SE_Matrix = np.zeros( (y, x) )
 SEM_Matrix = np.zeros( (y, x) )
 
+# fyld matricer til PLOTTING
 for i in range(y):                          # gå igennem hver linje på perspexpladen
     for j in range(x):                      # gå igennem hvert punkt på perspexpladen
         ya[j] = Mean_Of_Total[j + i*(x)]    # fyld 6 arrays med gennemsnittene af alle målinger
@@ -167,13 +176,13 @@ norm_wi = np.zeros_like(D)          # norm without index i
 NormArray = np.zeros( (m, n*N) )    # NormArray
 NormArray_wi = np.zeros( (m, n*N) ) # NormArray without index i
 
+# udregn normaliserede værdier og fyld matricer til PLOTTING
 for i in range(n*N):                                                        # hele datasættet igennem (90 punkter)
     norm[i] = D[i]/np.mean(D[i])                                            # normaliser alle målinger ift eget punktsæt
     for j in range(m):                                                      # 5 eller 10 gange, dvs en gang pr målingsindeks
         norm_wi[i] = D[i]/np.mean(np.concatenate( (D[i,:j], D[i,j+1:]) ))   # normaliser måling ift resterende målinger (ikke egen måling)
         NormArray[j,i] = norm[i,j]                                          # array med til plotting
         NormArray_wi[j,i] = norm_wi[i,j]                                    # array med til plotting
-
 
 #### LILLE PLOT MED KALIBRERINGSFAKTOR FOR FORSKELLIGE EKSPONERINGSTIDER ####
 first_meas_norms = np.array([1.024, 1.023, 1.015, 1.011 ])
@@ -241,10 +250,14 @@ axb[0].imshow(DM_Matrix, vmin=D_min, vmax=D_max, cmap='inferno',interpolation='l
 axb[0].set_title("Gennemsnitsdosis (mGy)")
 axb[0].invert_yaxis()
 axb[1].imshow(SE_Matrix, vmin=S_min, vmax=S_max, cmap=plt.cm.Blues,interpolation='lanczos')
-axb[1].set_title("Standard error (mGy),\n$SE=\\frac{np.std([x_1,...,x_{%s}])}{√%s}$  "%(int(n*m), int(n*m)))
+axb[1].set_title("Standard error (mGy),\n SE = $\\sqrt{ (\\frac{ SD(D_i) }{ \\sqrt{n\\cdot m} })^2 + \\overline{∆D}^2} $  ")
+#SE = $\\sqrt{\\frac{SD(x_i)^2 + (\overline{∆D})^2}{{m\\cdot n}}}$")
+#SE = $\\frac{np.std([x_1,...,x_{%s}])}{√%s}$  "%(int(n*m), int(n*m)))
 axb[1].invert_yaxis()
 axb[2].imshow(SEM_Matrix, vmin=S_min, vmax=S_max, cmap=plt.cm.Greens,interpolation='lanczos')
-axb[2].set_title("Standard error of the mean (mGy),\n $SEM=\\frac{np.std([\\overline{X}_1,...,\\overline{X}_{%s}])}{√%s}$ " %(n,n))
+axb[2].set_title("Standard error of the mean (mGy),\n SEm = $\\sqrt{ (\\frac{ SD(\overline{D}_j) }{ \\sqrt{n} })^2 + \\langle\overline{∆D}\\rangle^2} $  ")
+#SEM = $\\sqrt{\\frac{SD(\overline{X}_j)^2 + (\overline{∆D})^2}{{n}}}$")
+#SEM = $\\frac{np.std([\\overline{X}_1,...,\\overline{X}_{%s}])}{√%s}$ " %(n,n))
 axb[2].invert_yaxis()
 for i in range(x):
     for j in range(y):
@@ -327,7 +340,6 @@ axe.legend()
 plt.tight_layout(rect=titlecorrection)
 plt.savefig("Kalib_faktor_over_tid.pdf")
 
-
 #### PRINT CHRG OG DOSE MÅLT I HVERT PUNKT ####
 # for i in range(n*N):
 #     if x[i]==0: #lav mellemrum i udskriften mellem hver kolonne på perspex-pladen
@@ -339,24 +351,18 @@ plt.savefig("Kalib_faktor_over_tid.pdf")
 #         print()
 #     print ("I feltet (%s,%s) er dosen i gennemsnit ="  %( int(x[i]), int(y[i]) ),"%.2f"%DOSE_AVG[i], "±","%.2f"%DOSE_SEM[i], "mGy")
 # print()
-
-
-
-
+print()
 print("D[0] =", D[0])
 print("n*N =", n*N)
 print("m =", m)
-# print ("n*m =", n*D.shape[1])
 print ("x =", x )
 print ("y =", y)
 print ("N = y*x =", N)
 print ("n =", n)
-
-
+print( "n*x*y =", n*x*y)
 
 print()
 print("antal forsøg kørt: %s " %(n))
 print()
-
 
 plt.show()
